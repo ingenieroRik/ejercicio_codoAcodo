f31641e97bd1eaabf23c09d79cfdf0b5dbe73882
rik <ingeniero_rik@hotmail.com>
2022-10-19 23:33:59 -0300

    agregado de .js

--- index.js
+++ index.js
@@ -10,3 +10,79 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
+
+const precio = 200;
+const descuentoEstudiante = 0.8;
+const descuentoJunior = 0.85;
+const descuentoTrainee = 0.5;
+
+
+function borrarTodo(){
+  document.getElementById("cantidad").value = 0;
+  document.getElementById("totalPagar").value = "Total a pagar:$0";
+  //document.getElementById("nombre").value= "Nombre";
+  //document.getElementById("apellido").value = "Apellido";
+  //document.getElementById("correo").value= "correo";
+  return;
+}
+
+function calculaResumen(){
+      let cantidad=parseInt(document.getElementById("cantidad").value);   
+      let categoria = parseInt(document.getElementById("categoria").value);
+
+      
+      // verificar si lleno todos los campos
+     if (categoria != 1 && categoria != 2 && categoria != 3 && categoria !=4){
+        
+          alert("Por favor, seleccioná una categoría.");
+          return;
+      }
+
+
+      let nombre = document.getElementById("nombre");
+      let apellido = document.getElementById("apellido");
+      let correo = document.getElementById("correo");
+
+      
+
+      if ( nombre.value == ""){
+        alert("Por favor, escribí tu nombre");
+        nombre.focus();
+        return;
+      }if ( apellido.value == ""){
+        alert("Por favor, escribí tu apellido");
+        apellido.focus();
+        return;
+      }
+      if ( correo.value == ""){
+        alert("Por favor, escribí tu correo");
+        correo.focus();
+        return;
+      }
+
+      let valor = 0;
+      switch (categoria){
+              case 1:
+                  valor = precio * descuentoEstudiante;
+                  break;
+              case 2:
+                   valor = precio * descuentoTrainee;
+                   break;
+              case 3:
+                   valor = precio * descuentoJunior;
+                   break;
+              case 4:
+                    valor = precio ;
+                    break;
+              default:
+                   break;
+      }
+      let totalPagar = cantidad * valor;
+  
+     document.getElementById("totalPagar").innerText = "Total a pagar: $" + totalPagar;
+      return;
+}
+
+
+botonBorrar.addEventListener('click', borrarTodo);
+botonResumen.addEventListener('click', calculaResumen);
